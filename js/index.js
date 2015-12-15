d8411f889b8d10eaf381cefa4b90ab8ec56112bf
Janita Chalam <janitachalam@gmail.com>
2015-12-15 01:30:25 -0500

    clean components

--- js/index.js
+++ js/index.js
@@ -1,11 +1,31 @@
 var pageRank = (function() {
 	$(document).ready(function() {
+		$("#graph_button").click(function() {
+			$("#results_list").hide();
+			$("#vector_display").hide();
+			$("#link_graph").show();
+		});
+
+		$("#vector_button").click(function() {
+			$("#results_list").hide();
+			$("#link_graph").hide();
+			$("#vector_display").show();
+		});
+
+		$("#rank_button").click(function() {
+			$("#results_list").hide();
+			$("#link_graph").hide();
+			$("#vector_display").hide();
+		});
+
 		$("#search_button").click(function() {
 			$("#search_bar").val("");
+			$("#link_graph").hide();
+			$("#vector_display").hide();
 			var pageDivs = $(".list-group-item");
 			$(pageDivs).each(function(index){
-				$(this).html(pages[index].title);
-				$(this).attr("href", pages[index].link);
+				$(this).find('a').html(pages[index].title);
+				$(this).find('a').attr("href", pages[index].link);
 				$("#results_list").show();
 			});
 		});
@@ -15,19 +35,19 @@ var pageRank = (function() {
 			}
 		});
 
-		$("#matrix_button").hover(function() {
-			$("#results_list").hide();
-			$("#link_graph").show();
-		}, function() {
-			$("#link_graph").hide();
-		});
+		// $("#graph_button").hover(function() {
+		// 	$("#results_list").hide();
+		// 	$("#link_graph").show();
+		// }, function() {
+		// 	$("#link_graph").hide();
+		// });
 
-		$("#vector_button").hover(function() {
-			$("#results_list").hide();
-			$("#vector_display").show();
-		}, function() {
-			$("#vector_display").hide();
-		});
+		// $("#vector_button").hover(function() {
+		// 	$("#results_list").hide();
+		// 	$("#vector_display").show();
+		// }, function() {
+		// 	$("#vector_display").hide();
+		// });
 
 	});
 
@@ -48,8 +68,9 @@ var pageRank = (function() {
 	var numPages = 3;
 	var pages = [
 	new Page("Michael Jackson, Heating and Plumbing Engineer - Facebook", "https://www.facebook.com/Michael-Jackson-Heating-and-Plumbing-Engineer-189975057767412/"),
-	new Page("Michael Jackson | The Official Michael Jackson Site", "http://www.michaeljackson.com/us/"),
-	new Page("Michael Jackson - Wikipedia, the free encyclopedia", "https://en.wikipedia.org/wiki/Michael_Jackson")];
+	new Page("Michael Jackson - Google+", "https://plus.google.com/+MichaelJackson/posts"),
+	new Page("Michael Jackson - Wikipedia, the free encyclopedia", "https://en.wikipedia.org/wiki/Michael_Jackson"),
+	new Page("keep-michaeling.com: Michael Jackson fanpage", "keep-michaeling.com: Michael Jackson fanpage")];
 	var vectorOne = [0.333, 0.333, 0.333];
 	var vectorTwo = [1.283, 1.056, 0.667];
 	var vectorThree = [1.2351, 0.96, 0.804];
